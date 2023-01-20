a9bef3e5171cc7b7d06cbd2538ee6296da5d653d
TylerS2000 <tylerschulten@gmail.com>
2023-01-20 18:54:43 -0500

    log in page changes when person is logged in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Login from './components/Login';
 import './styles/app.css';
+import { useState } from 'react';
 function App() {
+  const [user, setUser] = useState(null);
+  console.log(user)
   return (
 <div>
-  <Login/>
+  {user ? <h1>Welcome {user.username}</h1> : <Login setUser={setUser}/>}
 </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import useField from '../hooks/useField'
 import login from '../backend'
 
 import '../styles/login.css'
-export default function Login(){
+export default function Login(props){
     const username = useField('text')
     const password = useField('password')
     return (
@@ -12,7 +12,7 @@ export default function Login(){
             <input type={username.type} value={username.value} onChange={username.onChange}/>
             <h2>Password</h2>
             <input type={password.type} value={password.value} onChange={password.onChange}/>
-            <button className="submit-btn" onClick={async(event)=>{event.preventDefault(); await login({username:username.value, password:password.value}) }}>Submit</button>
+            <button className="submit-btn" onClick={async(event)=>{event.preventDefault(); const user = await login({username:username.value, password:password.value}); props.setUser(1) }}>Submit</button>
         </div>
     )
 }
