ad7819cc466765827e760e0800aa61143583ba2f
TylerS2000 <tylerschulten@gmail.com>
2023-01-25 12:21:30 -0500

    popup added to post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,21 @@
 import getPosts from "../services/getPosts"
-import React, {useEffect} from "react"
+import React, {useEffect,useState} from "react"
 import Post from "./Post"
 export default function Home(props) {
+    const [popUp, setPopUp] = useState(false)
     const user = props.user
     const posts = props.posts
-
     useEffect(()=>getPosts(props.setPosts), [props.page, props.setPosts])
     return(
         <div>
             <h1  style={{color:"white"}}>Welcome {user.username}</h1>
            {posts.map((post)=>(
                <div key={post.id}>
-                    <p>{post.user}</p>
+                    <p>{post.user.username}</p>
                      <h2>{post.content}</h2>
                 </div>))}
-
-                <Post token = {user.token}/>
+                <button onClick={()=>setPopUp(!popUp)}>Post</button>
+                <Post token = {user.token} setPosts={props.setPosts} popUp={popUp} setPopUp={setPopUp}/>
         </div>
     )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,19 @@
 import post from '../services/post';
 import React from 'react';
 import useField from '../hooks/useField';
+import getPosts from '../services/getPosts';
+import '../styles/Post.css';
 export default function Post(props) {
 let content = useField("text")
 
-function handleClick(){post({content: content.value}, props.token)}
-    return(
-        <div>
+function handleClick(){post({content: content.value}, props.token); getPosts(props.setPosts)}
+    return(props.popUp) ? (
+        <div className='popup'>
+            <div className='popup-inner'>
             <input type="text" value = {content.value} onChange = {content.onChange}/>
             <button onClick={handleClick}>Post</button>
+            <button onClick={()=>props.setPopUp(false)}>Cancel</button>
         </div>
-    )
-}
+            </div>) : null}
+        
+    
