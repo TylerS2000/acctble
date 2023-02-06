f25562161b8abcdfa83f9a27bf80d51bca8ac70d
TylerS2000 <tylerschulten@gmail.com>
2023-02-06 10:00:34 -0500

    hmburger update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,16 @@
 import  "../styles/Navbar.css"
+import React from "react";
 export default function Navbar(props) {
+    const [change, setChange]=React.useState("")
+    function handleClick(e){
+        e.preventDefault();
+        if(change){
+            setChange("")
+        }
+        else(
+        setChange("change")
+        )
+    }
     return(
         <div className="navbar">
             <h1>Acctble.</h1>
@@ -8,11 +19,13 @@ export default function Navbar(props) {
             <h2 onClick={()=>props.setPage("profile")}>Profile</h2>
             <h2 onClick={()=>props.setPage("settings")}>Settings</h2>
             </div>
-            <div className="hamburger">
-            <div class="bar1"></div>
-            <div class="bar2"></div>
-            <div class="bar3"></div>
+            {change &&  <div className="nav-pop"></div>}
+            <div className={`hamburger ${change}`} onClick={handleClick}>
+            <div className={`bar1`}></div>
+            <div className={`bar2`}></div>
+            <div className={`bar3`}></div>
             </div>
+            
         </div>
     )
 }
